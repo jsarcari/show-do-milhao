050f3728947c949a431a863b48324e3d9553707e
Juan Arcari <39750973+jsarcari@users.noreply.github.com>
2024-07-04 04:21:31 -0300

    can skip in controller

---
+++ src/main/java/showdomilhao/MainController.java
@@ -0,0 +1,86 @@
+package showdomilhao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import showdomilhao.model.Answer;
+import showdomilhao.model.Guests;
+import showdomilhao.model.Participant;
+import showdomilhao.model.Question;
+import showdomilhao.service.ReadApi;
+
+@Controller
+public class MainController {
+
+    private ReadApi api;
+    private ArrayList<Question> questions;
+    private Answer answer;
+    private int indexQuestion;
+    private List<Integer> idsChoice;
+    private int index = 0;
+    private Participant user;
+    private Guests guests;
+
+    @GetMapping("/")
+    public ModelAndView indexPage() {
+        ModelAndView mv = new ModelAndView();
+        this.api = new ReadApi();
+        this.questions = this.api.getData();
+        this.user = new Participant(null);
+        this.answer = new Answer(1000);
+        this.guests = new Guests();
+        this.idsChoice = new ArrayList<Integer>();
+        mv.setViewName("name.jsp");
+
+        return mv;
+    }
+
+    @PostMapping("/question")
+    public ModelAndView questionPage(Participant participant, @RequestParam int id, String guestsAvailable, String skipAvailable) {
+        ModelAndView mv = new ModelAndView();
+        this.index = id-1;
+        this.indexQuestion = this.answer.choiceQuestion(this.questions, this.idsChoice, this.index);
+        this.idsChoice.add(this.indexQuestion);
+        Question question = this.questions.get(this.indexQuestion);
+        List options = this.answer.createArrayOptions(question);
+        if(this.index!=0) {
+            this.answer.setPremiumStop(this.answer.getPremium());
+            this.answer.setPremiumMiss(this.answer.getPremium()/2);
+            this.answer.calculatePremium(this.index);
+        } else {
+            this.user = participant;
+        }
+        if (guestsAvailable!=null && guestsAvailable.equals("false")) {
+            this.guests.setAvailable(false);
+        }
+        if (skipAvailable!=null && !skipAvailable.equals("")) {
+            this.user.setCanSkip(Integer.valueOf(skipAvailable));
+        }
+        
+        String valuePremium = String.format("%.0f", this.answer.getPremium());
+        String valueWrong = String.format("%.0f", this.answer.getPremiumMiss());
+        String valueStop = String.format("%.0f", this.answer.getPremiumStop());
+
+        mv.addObject("VALUE_PREMIUM", valuePremium);
+        mv.addObject("VALUE_WRONG", valueWrong);
+        mv.addObject("VALUE_STOP", valueStop);
+        mv.addObject("PARTICIPANT", this.user);
+        mv.addObject("QUESTION", question.getQuestion());
+        mv.addObject("ANSWER_ONE", options.get(0));
+        mv.addObject("ANSWER_TWO", options.get(1));
+        mv.addObject("ANSWER_THREE", options.get(2));
+        mv.addObject("ANSWER_FOUR", options.get(3));
+        mv.addObject("CORRECT_ANSWER", question.getCorrect_answer());
+        mv.addObject("GUESTS", this.guests);
+        mv.setViewName("question.jsp");
+
+        return mv;
+
+    }
+}
