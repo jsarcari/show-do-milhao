3c1c4e52db5126249fb4e53d0bf765123928b500
Juan Dos Santos Arcari <Juan.Arcari@zaffari.com.br>
2024-07-11 18:21:32 -0300

    change index in test premium tenth correct answer

--- src/test/java/showdomilhao/model/AnswerTest.java
+++ src/test/java/showdomilhao/model/AnswerTest.java
@@ -10,7 +10,7 @@ public class AnswerTest {
     @DisplayName("Prêmio para a décima resposta correta")
     void testPremiumTenthCorrectAnswer() {
         Answer answer = new Answer(50000);
-        int i = 9;
+        int i = 10;
         answer.calculatePremium(i);
 
         Assertions.assertEquals(100000,answer.getPremium());
